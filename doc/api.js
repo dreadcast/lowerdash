ccb43d6acc769c9025275dd7632e4d4113d49276
Raphaël Gully <concept@dreadcast.com>
2015-03-19 22:40:03 +0100

    Release 0.2.2

---
+++ doc/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Lowerdash"
+    ],
+    "modules": [
+        "Lodash"
+    ],
+    "allModules": [
+        {
+            "displayName": "Lodash",
+            "name": "Lodash",
+            "description": "Useful mixins for Lodash"
+        }
+    ]
+} };
+});
